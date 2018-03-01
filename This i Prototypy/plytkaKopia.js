3650d9ec2a785bbd500ffac94519205c8e8a4fce
LENOVO-BARTOSZ\Bartosz <b.mackiewicz1@gmail.com>
2018-03-01 17:52:30 +0100

    plytka kopia added

---
+++ This i Prototypy/plytkaKopia.js
@@ -0,0 +1,19 @@
+var foo = function() {
+    consol.log("hello");
+}
+
+var myObj = {
+    first: 1,
+    second: 2,
+    third: 3,
+    obj: {a:3, b:4},
+    foo: foo
+}
+
+var newObj = Object.assign({},myObj);
+console.log(newObj);
+
+newObj.obj.a = 4;
+
+console.log(newObj.obj.a)
+console.log(myObj.obj.a)
